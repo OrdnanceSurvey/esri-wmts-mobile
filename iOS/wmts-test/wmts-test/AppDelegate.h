c4ced83c30f7c665a984b146c9fd1a0b146cbcfe
drhaynes <drhmbp@gmail.com>
2016-01-29 11:42:59 +0000

    Initial commit.

---
+++ iOS/wmts-test/wmts-test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  wmts-test
+//
+//  Created by David Haynes on 29/01/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder<UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/wmts-test/wmts-test/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  wmts-test
+//
+//  Created by David Haynes on 29/01/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
